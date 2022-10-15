65aad19a1a0ee0a8d122b07125d5d454881be131
Rahul Sharma <rahulsharma94595@gmail.com>
2022-10-16 01:01:34 +0530

    Initial setup for the game

--- App.js
+++ App.js
@@ -1,62 +1,19 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, View, FlatList, Button} from 'react-native';
-import GoalInput from './components/GoalInput';
-import GoalItems from './components/GoalItems';
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
 
-const App = () => {
-  const [courseGoals, getCourseGoals] = useState([]);
-  const [modalVisible, setModalVisibility] = useState(false);
-
-  function submitData(name) {
-    getCourseGoals(currentCourseGoal => [
-      ...currentCourseGoal,
-      {text: name, id: Math.random().toString()},
-    ]);
-    dismissModal();
-  }
-
-  function deleteData(id) {
-    getCourseGoals(currentCourseGoal => {
-      return currentCourseGoal.filter(goal => goal.id !== id);
-    });
-  }
-  function addNewGoals() {
-    setModalVisibility(true);
-  }
-  function dismissModal() {
-    setModalVisibility(false);
-  }
-
+export default function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <Button title="Add new goal" onPress={addNewGoals} />
-        <GoalInput
-          visible={modalVisible}
-          onAddGoal={submitData}
-          cancelModal={dismissModal}
-        />
-        <View style={styles.goalContainer}>
-          <FlatList
-            data={courseGoals}
-            renderItem={itemData => {
-              return (
-                <GoalItems textOrId={itemData.item} onDeleteGoal={deleteData} />
-              );
-            }}
-            keyExtractor={(item, index) => {
-              return item.id;
-            }}
-          />
-        </View>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Hello World!!!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
+
 const styles = StyleSheet.create({
-  goalContainer: {
-    height: 500,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import {StyleSheet, TextInput, View} from 'react-native';
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      Buttons
+    </View>
+  );
+}
+export default StartGameScreen;
